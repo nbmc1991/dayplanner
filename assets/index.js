a7910a246a83c72576f211e1f5e46b361e565d54
nbmc1991 <nbmc1991@gmail.com>
2020-10-14 19:01:57 -0400

    index.html

--- assets/index.js
+++ assets/index.js
@@ -1,2 +1,90 @@
-var dayPlanner = document.getElementById("day-planner")
-console.log(dayPlanner)
+$(document).ready(function () {
+
+
+    //get times from moment
+    let now = moment().format("MMMM Do YYYY");
+    let nowDisplay = moment().format('MMMM Do YYYY')
+    //assigning where timewill be dislayed
+    let currentDate = $("#currentDay");
+    currentDate.text(nowDisplay)
+    console.log(currentDate)
+    // console.log(test)
+
+    function changeColor() {
+        var currentHour = moment().hours();
+
+        $(".time-block").each(function () {
+            var timeBlockh = parseInt($(this).attr("id"));
+            if (timeBlockh < currentHour) {
+                $(this).addClass("past")
+            } else if (timeBlockh === currentHour) {
+                $(this).removeClass("past")
+                $(this).addClass("present")
+            } else {
+                $(this).removeClass("past")
+                $(this).removeClass("present")
+                $(this).addClass("future")
+            }
+
+        });
+    }
+
+    changeColor();
+
+    $(".saveBtn").on("click",function(){
+        var todo = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id")
+        localStorage.setItem(time, todo);
+
+    });
+
+    $("#9 .description").val(localStorage.getItem("9"))
+
+    // let test = false;
+
+    // let plannerForm = $("#formPlanner");
+
+    // plannerForm.empty();
+
+    // let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
+
+    // if (test) {console.log("storedPlans"); 
+    // }
+
+
+
+
+
+
+    // for (var time = 9; time < 18; time++)
+    // console.log(time)
+
+    // let rowDiv = $("<div>");
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
