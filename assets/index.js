c951b15ca39f26eb55239048abea5c0bba16fb89
nbmc1991 <70964726+nbmc1991@users.noreply.github.com>
2020-10-14 17:19:47 -0400

    Add files via upload

--- assets/index.js
+++ assets/index.js
@@ -1,2 +1,27 @@
-var dayPlanner = document.getElementById("day-planner")
-console.log(dayPlanner)
+//get times from moment
+let now = moment().format("MMMM Do YYYY");
+let nowDisplay=moment().format('MMMM Do YYYY')
+//assigning where timewill be dislayed
+let currentDate = $("#currentDay");
+currentDate.text(nowDisplay)
+console.log(currentDate)
+    // console.log(test)
+
+let storedTodos = JSON.parse(localStorage.getItem("storedTodos"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
