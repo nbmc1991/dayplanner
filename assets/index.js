4e2ae9c4383c864b5a83dd590a749fb969b1b503
norma <nbmc1991@gmail.com>
2020-10-17 13:07:24 -0400

    upadates

--- assets/index.js
+++ assets/index.js
@@ -3,13 +3,15 @@ $(document).ready(function () {
 
     //get times from moment
     let now = moment().format("MMMM Do YYYY");
-    let nowDisplay = moment().format('MMMM Do YYYY')
+    let nowDisplay = moment().format(' dddd, MMMM/Do/YYYY ')
     //assigning where timewill be dislayed
     let currentDate = $("#currentDay");
     currentDate.text(nowDisplay)
     console.log(currentDate)
     // console.log(test)
 
+    
+    // function to change color based on what time it is
     function changeColor() {
         var currentHour = moment().hours();
        
@@ -29,7 +31,6 @@ $(document).ready(function () {
 
         });
     }
-
     changeColor();
 
     $(".saveBtn").on("click",function(){
@@ -39,35 +40,21 @@ $(document).ready(function () {
 
     });
 
+    //target each time block item and save value to local storage
     $("#9 .description").val(localStorage.getItem("9"))
-    $("#9 .description").val(localStorage.getItem("9"))
-    $("#9 .description").val(localStorage.getItem("9"))
-    $("#9 .description").val(localStorage.getItem("9"))
-    $("#9 .description").val(localStorage.getItem("9"))
-    $("#9 .description").val(localStorage.getItem("9"))
-
-
-
-    // let test = false;
-
-    // let plannerForm = $("#formPlanner");
-
-    // plannerForm.empty();
-
-    // let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
-
-    // if (test) {console.log("storedPlans"); 
-    // }
+    $("#10 .description").val(localStorage.getItem("10"))
+    $("#11 .description").val(localStorage.getItem("11"))
+    $("#12 .description").val(localStorage.getItem("12"))
+    $("#13 .description").val(localStorage.getItem("13"))
+    $("#14 .description").val(localStorage.getItem("14"))
+    $("#15 .description").val(localStorage.getItem("15"))
+    $("#16 .description").val(localStorage.getItem("16"))
+    $("#17 .description").val(localStorage.getItem("17"))
 
 
 
 
-
-
-    // for (var time = 9; time < 18; time++)
-    // console.log(time)
-
-    // let rowDiv = $("<div>");
+    
 
 
 
